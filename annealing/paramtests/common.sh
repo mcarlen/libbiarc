fca2fc6d0da636aa3f6952c7aa0346c10b9ce2a4
Mathias Carlen <mathias.carlen@epfl.ch>
2008-11-14 11:39:57 +0100

    param testing with qsub

---
+++ annealing/paramtests/common.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function check {
+  if ! [ -e $1 ]; then
+    echo "[Err] $1 not found."
+    exit 1
+  fi
+}
+
+function gensub {
+echo "qsub file : $3"
+if [ "$#" != "3" ]; then
+  echo "[Err] gensub : jobname commandstring filename"
+  exit 1
+fi
+cat <<EOF >$3
+#!/bin/bash
+#$ -S /bin/bash
+#$ -cwd
+#$ -N $1
+#$ -e /dev/null
+#$ -o /dev/null
+$2
+EOF
+}
+
--- annealing/paramtests/paramplot.sh
+++ annealing/paramtests/paramplot.sh
@@ -1,39 +1,24 @@
 #!/bin/bash
 
-if [ "$#" != "9" ]; then
-  echo "Usage : $0 knot T0 Ts T1 C0 Cs C1 time_in_s outfile.png"
-  echo "example : k3.1.pkf 0.01 .005 0.02 0.001 .0005 0.002 10 out.png"
+if [ "$#" != "2" ]; then
+  echo "Usage : $0 knot log_dir"
   exit 0
 fi
+
+source common.sh
 
 knotinfo=$LIBBIARC/tools/info
 # this modified annealing prog prints elapsed time and thickness/energy
 # on std out
 anneal=$LIBBIARC/annealing/paramtests/anneal_param
 plotit=`pwd`/plotit.sh
-postproc=`pwd`/postproc.sh
 AnnealFlag=1
 LogFreq=1
 Seed=0
-log_dir=/tmp/log_dir
 
 knot=$1
-T0=$2
-Ts=$3
-T1=$4
-C0=$5
-Cs=$6
-C1=$7
-tf=$8
-outfile=$9
+log_dir=$2
 prog=`basename $anneal`
-
-function check {
-  if ! [ -e $1 ]; then
-    echo "[Err] $1 not found."
-    exit 1
-  fi
-}
 
 check $knotinfo
 check $anneal
@@ -43,23 +28,20 @@ check $knot
 
 if ! [ -d $log_dir ]; then
   echo "Create log dir : $log_dir"
-  mkdir -p $log_dir
+  mkdir -p $log_dir/sub
 fi
 
 i=0
-for T in `seq $T0 $Ts $T1`; do
-  for C in `seq $C0 $Cs $C1`; do
+# Tlist="0.01 0.1 1"
+# Clist="1e-6 1e-5 1e-4 1e-3"
+Tlist="1 2"
+Clist="3 4"
+for T in $Tlist; do
+  for C in $Clist; do
+    li=`printf "%04d" $i`
     echo "Process T=$T C=$C"
-    $anneal $knot $log_dir/ $T $C $AnnealFlag $LogFreq 1e-12 $Seed >/dev/null 2>&1 &
-    sleep $tf
-    if [[ `pidof $prog` ]]; then
-      $(kill -HUP `pidof $prog` >/dev/null 2>&1)
-    fi
-    mv $log_dir/log $log_dir/log`printf "%04d" $i`
+    gensub anneal$li "$anneal $knot ${log_dir}/log$li $T $C $AnnealFlag $LogFreq 1e-12 $Seed >/dev/null 2>&1" ${log_dir}/sub/anneal$li.sub
+    echo qsub ${log_dir}/sub/anneal$li.sub
     i=$(( i+1 ))
   done
 done
-
-$plotit $log_dir $outfile
-
-rm -rf $log_dir
