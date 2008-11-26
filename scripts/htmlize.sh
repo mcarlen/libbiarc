980e7af0fd44be102c0d7e1ea65e7f0b61b1bb1f
Mathias Carlen <mathias.carlen@epfl.ch>
2008-11-27 00:34:44 +0100

    scripts directory with htmlize.sh
      * generate R3 plots (knotinfo,curvature,pp,pt,tt,rho)
      * same for S3/R4 curves
    
    Usage : htmlize.sh [4] knot.pkf out.html
    
    where the optional 4 tells if we have an R3 or R4 knot

---
+++ scripts/htmlize.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+function usage {
+  echo "Usage : $0 [4] file.pkf out.html"
+  exit 0
+}
+
+D=""
+if [ "$#" == "3" ]; then
+  if [ "$1" != "4" ]; then
+    usage
+  fi 
+  D=4
+  shift
+fi
+
+if [ "$#" != "2" ]; then
+  usage
+fi
+
+function check {
+  if ! [ -e $1 ]; then
+    echo "$1 not found."
+    exit 1
+  fi
+}
+
+knotfile=$1
+knot1=`basename $1`
+knot=${knot1%.pkf}
+
+# Tools needed
+source ~/.bashrc
+plot=$LIBBIARC/experimental/pngmanip/plot$D
+curv=$LIBBIARC/tools/curvature$D
+info=$LIBBIARC/tools/info$D
+
+check $plot
+check $curv
+check $info
+
+# Get knot info
+msg=($($info $1 | sed '1,5d'))
+name=$knot
+if [ "$D" == "4" ]; then
+  N=${msg[5]}
+  L=${msg[9]}
+  D=${msg[21]}
+  rope=${msg[25]}
+else
+  N=${msg[5]}
+  L=${msg[9]}
+  D=${msg[20]}
+  rope=${msg[24]}
+fi
+
+# generate curvature plots
+echo "Generate curvature plots"
+$curv $knotfile >$knot.curvature
+gnuplot <<EOF
+set term png
+set output "$knot-curvature.png"
+set title "Curvature/rho for $knot"
+plot [0:$L] [0:1] "$knot.curvature" using 3:4 with steps notitle, "$knot.curvature" using 3:5 with steps notitle
+set output "$knot-rho.png
+set title "rho for $knot"
+plot [0:$L] [.9:1] "$knot.curvature" using 3:5 with steps notitle
+EOF
+
+echo "pp/pt/tt plots"
+for i in pp pt tt; do
+  $plot -plot $i $1 >/dev/null
+  mv out.png $knot-$i.png
+done
+
+echo "Dump html"
+cat <<EOF >$2
+<html>
+  <h2>$name</h2>
+  <b>N=$N<br/>L=$L<br/>D=$D<br/>Rope=$rope<br/></b>
+  <TABLE><TR>
+    <TD>D/(2*radius_of_curvature) <IMG SRC='$knot-curvature.png'></TD>
+    <TD>2pp/D <IMG SRC='$knot-pp.png'></TD>
+    <TD>D/2pt <IMG SRC='$knot-pt.png'></TD>
+    <TD>D/2tt <IMG SRC='$knot-tt.png'></TD>
+    <TD>D/(2*rho) <IMG SRC='$knot-rho.png'></TD>
+  </TR></TABLE>
+</html>
+EOF
+
