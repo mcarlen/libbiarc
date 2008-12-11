6f6c713cf56b7809299d111920841f789adb3d4c
Henryk Gerlach <henryk.gerlach@epfl.ch>
2008-12-11 12:03:54 +0100

    Fixed? strange hang bug in anneal4; fixed build problem in coeff2pkf.

--- fourierknots/coeff2pkf.cpp
+++ fourierknots/coeff2pkf.cpp
@@ -1,16 +1,6 @@
 #include "fourier_3_1.h"
 #include "fourier_4_1.h"
 
-float adjust(float x) {
-  return x+0.9/(3.*2.*M_PI)*sin(3.*2.*M_PI*x);
-}
-
-float adjust2(float x) {
-  float s = 0.95;
-  return (x+s/(3.*2.*M_PI)*sin(3.*2.*M_PI*x) +
-         s/(3.*2.*M_PI)*sin(3.*2.*M_PI*(x+s/(3.*2.*M_PI)*sin(3.*2.*M_PI*x))));
-}
-
 // This is clunky (3 funcs for doing the same)
 Curve<Vector3> genn(const int N, const char* infile, const char* outfile) {
   Curve<Vector3> knot;
