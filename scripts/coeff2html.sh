bee36703cefb48859d82ac109dab485122ada1e8
Mathias Carlen <mathias.carlen@epfl.ch>
2009-02-13 14:23:35 +0100

    * curvature uses point to somewhere rhopt function (make the offset to 1    smaller)
     * coeff2html : torsion and curvature on same plot. angle plot. pp and tt on
       same plot

--- scripts/coeff2html.sh
+++ scripts/coeff2html.sh
@@ -32,6 +32,7 @@ tors=$LIBBIARC/tools/torsion$D
 info=$LIBBIARC/tools/info$D
 coeff2pkf=$LIBBIARC/fourierknots/coeff2pkf
 fcurv=$LIBBIARC/fourierknots/fcurvature
+angles=$LIBBIARC/tools/angle_principal_normal_contacts
 
 check $coeff
 check $plot
@@ -40,6 +41,7 @@ check $tors
 check $info
 check $coeff2pkf
 check $fcurv
+check $angles
 
 # How many coefficients?
 Ncoeffs=$(cat $coeff | wc -w)
@@ -71,13 +73,17 @@ echo "Generate curvature plots"
 $curv $knotfile >$knot.curvature
 $fcurv $style $res $coeff > $knot.fcurvature
 $tors $knotfile >$knot.torsion
+$angles $knotfile 0.001 >$knot.angles
 maxfcurv=$(cut -d" " -f2 $knot.fcurvature | sort -n | tail -n 1)
 
+echo "N=$N,L=$L"
 gnuplot <<EOF
 set term png
-set output "$knot-curvature.png"
-set title "Curvature/rho and arclengthparam for $knot"
-plot [0:$L] [0:1] "$knot.curvature" using 3:4 with steps notitle, "$knot.curvature" using 3:5 with steps notitle, "$knot.curvature" using (\$0/(2*$N)):3 with lines notitle
+set output "$knot-torsion-curvature.png"
+set title "Curvature/rho/arclengthparam and angle between biarcs for $knot"
+set xlabel "arclength"
+set ylabel "angle betw biarcs / 180 - rad/(2*D)"
+plot [0:$L] [0:1] "$knot.curvature" using 3:4 with steps notitle, "$knot.curvature" using 3:5 with steps notitle, "$knot.curvature" using (\$2/(2*$N)*$L):(\$3/$L) with lines notitle, "$knot.torsion" using 3:(\$5/180) with steps notitle
 set output "$knot-rho.png
 set title "rho for $knot"
 plot [0:$L] [.999:1] "$knot.curvature" using 3:5 with steps notitle
@@ -87,10 +93,11 @@ set xlabel "t"
 set ylabel "D/(2.*fourier''(t))"
 plot [0:1] [0:1] "$knot.fcurvature" using 1:($D/2.*\$2) with lines notitle
 set output "$knot-torsion.png"
-set title "Torsion for $knot"
+set output "$knot-angles.png"
+set title "Angles between principal normal and contact chords"
 set xlabel "arclength"
-set ylabel "Torsion(arclength)"
-plot "$knot.torsion" using 3:5 with lines notitle
+set ylabel "angle (rad)"
+plot "$knot.angles" using 1:((\$2*2)-1)*pi/2 notitle
 EOF
 
 echo "pp/pt/tt plots"
@@ -99,22 +106,28 @@ for i in pp pt tt; do
   mv out.png $knot-$i.png
 done
 
+# Composite tt and pp plot into one image
+convert -size 500x500 xc: -draw 'polygon 499,499 0,499, 0,0' mask.png
+composite $knot-tt.png $knot-pp.png mask.png $knot-pp-tt.png
+
 echo "Dump html to $4"
 cat <<EOF >$4
 <html>
   <h2>$name</h2>
   <b>Coeffs=$Ncoeffs,N=$res<br/>L=$L<br/>D=$D<br/>Rope=$rope<br/></b>
   <TABLE><TR>
-    <TD>D/(2*radius_of_curvature) <IMG SRC='$knot-curvature.png'></TD>
-    <TD>D/(2*rad_fourier_curv) <IMG SRC='$knot-fcurvature.png'></TD>
-    <TD>Torsion <IMG SRC='$knot-torsion.png'></TD>
-    <TD>2pp/D <IMG SRC='$knot-pp.png'></TD>
+    <TD>D/(2*radius_of_curvature) <IMG SRC='$knot-torsion-curvature.png'></TD>
+<!--    <TD>D/(2*rad_fourier_curv) <IMG SRC='$knot-fcurvature.png'></TD>
+    <TD>Angle between biarcs (~Torsion) <IMG SRC='$knot-torsion.png'></TD> -->
+<!--  <TD>2pp/D <IMG SRC='$knot-pp.png'></TD> -->
     <TD>D/2pt <IMG SRC='$knot-pt.png'></TD>
-    <TD>D/2tt <IMG SRC='$knot-tt.png'></TD>
+<!--    <TD>D/2tt <IMG SRC='$knot-tt.png'></TD> -->
+    <TD>PP (bottom) and TT (top) plot <IMG SRC='$knot-pp-tt.png'></TD>
     <TD>D/(2*rho) <IMG SRC='$knot-rho.png'></TD>
+    <TD>Angles <IMG SRC='$knot-angles.png'></TD>
   </TR></TABLE>
 </html>
 EOF
 
 # Clean-up
-rm -f $knot.curvature $knot.fcurvature $knotfile
+# rm -f $knot.curvature $knot.fcurvature $knotfile $knot.torsion $knot-pp.png $knot-tt.png mask.png
