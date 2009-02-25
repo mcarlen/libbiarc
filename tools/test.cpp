168b9c8fcd5884edbad3e864730086f4bbd097b1
Mathias Carlen <mathias.carlen@epfl.ch>
2009-02-25 23:28:51 +0100

    test is still there to generate a contact surface out of a contact line given as s sigma values (pt plot).
    The tool just got extended by the ability to output wavefront .obj files

--- tools/test.cpp
+++ tools/test.cpp
@@ -22,16 +22,120 @@ struct contact_double {
 typedef contact_double contact_double;
 typedef contact contact;
 
-vector<contact_double> raw;
-vector<contact> final, final2, final3;
-
 bool compare(const contact& a, const contact& b) {
   return a.s < b.s;
 }
 
-void dump_surface(ofstream& out, vector<contact> &final, Curve<Vector3> *k, int num, int close = 0) {
+// Contacts struts
+struct CStrut {
+  Vector3 p0, p1;
+};
+typedef CStrut CStrut;
+
+
+void getContacts(Curve<Vector3>& curve, vector<contact>& stcontacts, vector<CStrut> *contacts) {
+  vector<contact>::iterator it;
+  CStrut strut;
+  for (it=stcontacts.begin();it!=stcontacts.end();++it) {
+    strut.p0 = curve.pointAt(it->s);
+    if (it->t > 1)
+      strut.p1 = curve.pointAt(it->t-1);
+    else
+      strut.p1 = curve.pointAt(it->t);
+    contacts->push_back(strut);
+  }
+}
+
+void dump_obj_surface(ofstream& out, vector<CStrut>& contacts, unsigned int num, int close = 0) {
+
+  assert(num<contacts.size());
+  const int Segments = 30;
+
+  float step_size_1, step_size_2;
+  Vector3 N_direc_vect_1, N_direc_vect_2;
+  Vector3 save1, save2, V1, V2, V3, V4;
+
+  static int FaceIndex1 = 1, FaceIndex2 = 2, FaceIndex3 = 3;
   
-  assert(num<final.size());
+  // foreach contact line pair
+
+  //
+  // Main surface (Triangulation)
+  //
+
+  out << "g csurface" << endl;
+  float s,t,s1,t1;
+  for (unsigned int l=0;l< num + close;++l) {
+
+    // set 4 corners of the "rectangle"
+    V1 = contacts[l].p0;
+    V2 = contacts[l].p1;
+
+    V3 = contacts[(l+1)%contacts.size()].p0;
+    V4 = contacts[(l+1)%contacts.size()].p1;
+    
+    // compute step size
+    step_size_1 = (V2-V1).norm()/(float)Segments; //NO_OF_SUB_TRIS;
+    step_size_2 = (V4-V3).norm()/(float)Segments; //NO_OF_SUB_TRIS;
+    
+    // get the direction vector for each contact segment
+    N_direc_vect_1 = (V2-V1);
+    N_direc_vect_2 = (V4-V3);
+    
+    N_direc_vect_1.normalize();
+    N_direc_vect_2.normalize();
+
+    for (int i=0;i<Segments;i++) {
+
+      s = (float)(l)/(float)(contacts.size()-1);
+      t = (float)i/(float)(Segments-1);
+      s1 =(float)(l+1)/(float)(contacts.size()-1);
+      t1 = (float)(i+1)/(float)(Segments-1);
+
+      // for glm lib make it wrap
+      if(s==1) s=0;if(t==1)t=0;if(s1==1)s1=0;if(t1==1)t1=0;
+
+      // triangle 1
+      out << "v " << (V1 + (N_direc_vect_1*(i*step_size_1))) << endl;
+      out << "v " << (V3 + (N_direc_vect_2*(i*step_size_2))) << endl;
+      out << "v " << (V1 + (N_direc_vect_1*((i+1)*step_size_1))) << endl;
+
+      // texture coords
+      out << "vt " << s << " " << t << endl;
+      out << "vt " << s1 << " " << t << endl;
+      out << "vt " << s << " " << t1 << endl;
+
+      //out << "f -3 -2 -1" << endl;
+      out << "f "<< FaceIndex1 << "/" << FaceIndex1 << " "
+          << FaceIndex2 << "/" << FaceIndex2 << " "
+  	      << FaceIndex3 << "/" << FaceIndex3 << endl;
+
+      FaceIndex1+=3; FaceIndex2+=3; FaceIndex3+=3;
+      
+      // triangle 2
+      out << "v " << (V1 + N_direc_vect_1*((i+1)*step_size_1)) << endl;
+      out << "v " << (V3 + N_direc_vect_2*(i*step_size_2)) << endl;
+      out << "v " << (V3 + N_direc_vect_2*((i+1)*step_size_2)) << endl;
+
+      // texture coords
+      out << "vt " << s << " " << t1 << endl;
+      out << "vt " << s1 << " " << t << endl;
+      out << "vt " << s1 << " " << t1 << endl;
+
+      // out << "f -3 -2 -1" << endl;
+      out << "f "<< FaceIndex1 << "/" << FaceIndex1 << " "
+          << FaceIndex2 << "/" << FaceIndex2 << " "
+	        << FaceIndex3 << "/" << FaceIndex3 << endl;
+    
+      FaceIndex1+=3; FaceIndex2+=3; FaceIndex3+=3;
+	    
+    }
+  }
+}
+
+void dump_iv_surface(ofstream& out, vector<CStrut>& contacts, unsigned int num, int close = 0) {
+  
+  assert(num<contacts.size());
 
   // How many segments
   int Seg = 30;
@@ -58,9 +162,9 @@ void dump_surface(ofstream& out, vector<contact> &final, Curve<Vector3> *k, int 
 
   // Write a color for every slice (i.e. number of contacts)
   RGB col;
-  for (unsigned int i=0;i<final.size();++i) {
+  for (unsigned int i=0;i<contacts.size();++i) {
     map_color_rainbow_cycle(&col,
-        (float)i/(float)(final.size()-1),
+        (float)i/(float)(contacts.size()-1),
         0.,1.);
     out << b2f(col.r)/255. << " "
         << b2f(col.g)/255. << " "
@@ -74,19 +178,10 @@ void dump_surface(ofstream& out, vector<contact> &final, Curve<Vector3> *k, int 
 
   // Write out coordinates
   Vector3 v1, v2, v3, v4;
-  for (int j=0;j<num;++j) { // it=contacts.begin();it!=contacts.end();++it) {
-    v1 = k->pointAt(final[j].s);   v2 = k->pointAt(final[j].t);
-    v3 = k->pointAt(final[j+1].s); v4 = k->pointAt(final[j+1].t);
+  for (unsigned int j=0;j<num + close;++j) { // it=contacts.begin();it!=contacts.end();++it) {
 
-    for (int i=0;i<Seg;i++) {
-      out << v1+(v2-v1)*(float)i/(float)(Seg-1) << ",";
-      out << v3+(v4-v3)*(float)i/(float)(Seg-1) << ",";
-    }
-  }
-
-  if (close) {
-    v1 = k->pointAt(final[final.size()-1].s); v2 = k->pointAt(final[final.size()-1].t);
-    v3 = k->pointAt(final[0].s);              v4 = k->pointAt(final[0].t);
+    v1 = contacts[j].p0;   v2 = contacts[j].p1;
+    v3 = contacts[(j+1)%contacts.size()].p0; v4 = contacts[(j+1)%contacts.size()].p1;
 
     for (int i=0;i<Seg;i++) {
       out << v1+(v2-v1)*(float)i/(float)(Seg-1) << ",";
@@ -111,6 +206,7 @@ int main(int argc, char **argv) {
     return 0;
   }
 
+  vector<contact_double> raw;
   contact_double c2;
   ifstream in(argv[2],ios::in);
   while (in >> c2.s >> c2.sigma >> c2.tau)
@@ -119,43 +215,44 @@ int main(int argc, char **argv) {
 
   // double the stuff
   contact c;
-  for (int i=0;i<raw.size();i++) { // ++i) {
+  vector<contact> final, final2;
+  for (unsigned int i=0;i<raw.size();i++) {
     c.s = raw[i].s;
     c.t = raw[i].sigma;
     final.push_back(c);
+
 /*
     c.s = raw[i].tau;
     c.t = raw[i].s;
     final.push_back(c);
-    */
+*/
   }
-  
-  // sort final by s
-  sort(final.begin(),final.end(),compare);
 
-  // remove contacts that do not fit :D
-  float tol = 10; // if diff > 1% drop contact
-  float tol2 = 0.0;
+  // sort final by s
+//  sort(final.begin(),final.end(),compare);
+
   float ot = final[0].t, os = final[0].s;
   float s, t, val;
   c.s = os; c.t = ot;
   final2.push_back(c);
   cerr << os << " " << ot << endl;
 
-  for (int i=1;i<final.size();i++) {
+  for (unsigned int i=1;i<final.size();i++) {
 
     s = final[i].s; t = final[i].t;
-    if (s>t) t += 1;
-    if (t>1) t -= 1;
+    if (s>1. || s<0.) continue;
+//    if (s>t) t += 1;
+
     val = (os-s)*(os-s) + (ot-t)*(ot-t);
 
     // We want a monotone function
-    if (ot>t) continue;
+    if (ot>t) {
+      cout << "Not monotonic s=" << s << ",t=" << t << ",ot=" << ot << '\n';
+      continue;
+    }
 
-//    if (val < 0.00001) continue;
- //   if (fabsf(t - ot) < tol && val > tol2) {
+    if (val < 0.0001) continue;
 
-      // cerr << final[i].s << " " << final[i].t << endl;
       cerr << s << " " << t << endl;
       
       c.s = final[i].s; c.t = final[i].t;
@@ -163,7 +260,6 @@ int main(int argc, char **argv) {
       
       os = s;
       ot = t;
-//    }
   }
 
   // Dump iv surface
@@ -172,22 +268,43 @@ int main(int argc, char **argv) {
   k->normalize();
   k->make_default();
 
+  // Write s sigma contacts to Vector3 contacts
+  vector<CStrut> contacts;
+  getContacts(*k, final2, &contacts);
+
   char name[100];
   /*
   for (int nn=1;nn<final2.size()-1;nn++) {
     sprintf(name,"surf_%04d.iv", nn);
     ofstream out(name,ios::out);
-    dump_surface(out, final2, k, nn);
+    dump_iv_surface(out, contacts, nn);
     out.close();
   }
-  */
 
-  sprintf(name,"surf_%04d.iv", final2.size()-1);
+  */
+  sprintf(name,"surf_%04d.iv", contacts.size()-1);
   ofstream out(name,ios::out);
-  dump_surface(out, final2, k, final2.size()-1, 1);
+  dump_iv_surface(out, contacts, contacts.size()-1, 1);
   out.close();
 
+/*
+  sprintf(name,"surf.obj");
+  ofstream out(name,ios::out);
+  dump_obj_surface(out, contacts, contacts.size()-1, 1);
+  out.close();
+  */
+
+  // write contacts.txt file
+  // format : p0x p0y p0z midpx midpy midpz p1x p1y p1z
+  out.open("contacts.txt",ios::out);
+  for (unsigned int i=0;i<contacts.size();++i) {
+    out << contacts[i].p0 << " "
+        << (contacts[i].p0+contacts[i].p1)*.5 << " "
+        << contacts[i].p1 << endl;
+  }
+
   delete k;
+  contacts.clear();
   return 0;
 }
 
