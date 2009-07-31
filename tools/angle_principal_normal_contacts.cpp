af5a42881c1ff67bf2f30e7f6524081d05c41065
Mathias Carlen <mathias.carlen@epfl.ch>
2009-07-31 03:42:30 +0200

    print s sigma for the contactset. working.

--- tools/angle_principal_normal_contacts.cpp
+++ tools/angle_principal_normal_contacts.cpp
@@ -58,6 +58,59 @@ float arclength(Curve<Vector3> &c, int n) {
   return s;
 }
 
+float find_s(Curve<Vector3> &c, Vector3 &p) {
+  // XXX possible optimization : not necessary to check against all the points ...
+  // Find closest point to p on curve c
+  float d = 1e22, dl; b3 close;
+  for (b3 b=c.begin();b!=c.end();++b) {
+    dl = (b->getPoint()-p).norm2();
+    if (dl < d) {
+      d = dl;
+      close = b;
+    }
+  }
+
+  b3 neighbor;
+  if ( (close->getPoint()-close->getNext().getPoint()).norm2() < (close->getPoint()-close->getPrevious().getPoint()).norm2() ) {
+    if (close == c.begin())
+      neighbor = c.end()-1;
+    else
+      neighbor = close-1;
+  }
+  else {
+    if (close == c.end()-1)
+      neighbor = c.begin();
+    else
+      neighbor = close+1;
+  }
+
+  // recover s for our point p
+  float s0 = arclength(c,close->id());
+  float s1 = arclength(c,neighbor->id());
+
+  if (s0>s1) {
+    float tmp = s0;
+    s0 = s1; s1 = tmp;
+  }
+
+  d = 1e22;
+  float c_s0 = s0, c_s1 = s1, s_best = s0;
+  Vector3 ptrial;
+
+  // Make this bisection better!!!
+  for (int i=0;i<100;++i) {
+    ptrial = c.pointAt(c_s0 + (c_s1-c_s0)*(float)i/99.);
+    dl = (ptrial - p).norm2();
+    if (dl<d) {
+      d = dl;
+      s_best = c_s0 + (c_s1-c_s0)*(float)i/99.;
+    }
+  }
+
+  return s_best;
+
+}
+
 /*
   Convert angle from acos() to interval [0,1]
 */
@@ -106,7 +159,7 @@ int main(int argc, char **argv) {
   */
 #define b3 vector<Biarc<Vector3> >::iterator
   Vector3 b0,b1,b2,p,v,CDir,tan,nor;
-  float r, slen;
+  float r, slen, sigma;
   int ContactN = 0;
   for (int i=0;i<N;++i) {
     slen = arclength(c,i)/L;
@@ -128,14 +181,16 @@ int main(int argc, char **argv) {
       r = it->radius0();
       if (rhopt(p,b0,b1,b2,r,v) && th_cond(thick,p,v,tol)) {
         CDir = (v-p); CDir.normalize();
-        cout << slen << " " << myangle(nor,CDir,tan) << endl;
+        sigma = find_s(c,v);
+        cout << slen << " " << myangle(nor,CDir,tan) << " " << sigma/L << endl;
         ++ContactN;
       }
       it->getBezierArc1(b0,b1,b2);
       r = it->radius1();
       if (rhopt(p,b0,b1,b2,r,v) && th_cond(thick,p,v,tol)) {
         CDir = (v-p); CDir.normalize();
-        cout << slen << " " << myangle(nor,CDir,tan) << endl;
+        sigma = find_s(c,v);
+        cout << slen << " " << myangle(nor,CDir,tan) << " " << sigma/L << endl;
         ++ContactN;
       }
     }
